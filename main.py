9b643fe608e1b2b7473b2397deb1919893c5a7df
ThomasLin <linziyue.nsfz@outlook.com>
2021-12-23 13:59:17 -0500

    Read in data

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read in data
+test = pd.read_csv("./test.csv")
+train = pd.read_csv("./train.csv")
+gender_submission = pd.read_csv("./gender_submission.csv")
+
+
+print(test.head(5))
+print(train.head(5))
+print(gender_submission.head(5))
