5df7d0f930727f09e0f44de0b1713d31288f4dd9
ThomasLin <linziyue.nsfz@outlook.com>
2021-12-31 00:25:34 -0500

    Completed a rough version that got me ranked no. 12266

--- titanic.py
+++ titanic.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
@@ -7,21 +8,37 @@ from sklearn.ensemble import RandomForestRegressor
 # Read in data
 test_data = pd.read_csv("./test.csv")
 train_data = pd.read_csv("./train.csv")
+# Data cleaninig
+# Fill the missing Age with the average Age in train_data
+train_age_mean = train_data.dropna(axis=0).Age.mean()
+train_data.Age = train_data.Age.fillna(value = train_age_mean)
+# Turn Sex into binary variate
+gender = pd.get_dummies(train_data["Sex"])
+train_data = pd.concat((train_data, gender), axis=1)
+train_data = train_data.drop(["Sex", "female"], axis=1)
+train_data = train_data.rename(columns={"male":"Sex"})
 
-# Filter rows with missing values
-train_data = train_data.dropna(axis=0)
-test_data = test_data.dropna(axis=0)
+# Do the same for test_data
+test_age_mean = test_data.dropna(axis=0).Age.mean()
+test_data.Age = test_data.Age.fillna(value = test_age_mean)
+test_gender = pd.get_dummies(test_data["Sex"])
+test_data = pd.concat((test_data, test_gender), axis=1)
+test_data = test_data.drop(["Sex", "female"], axis=1)
+test_data = test_data.rename(columns={"male":"Sex"})
 
-# Choose target and features
+fare_mean = test_data.dropna(axis=0).Fare.mean()
+test_data.Fare = test_data.Fare.fillna(value = fare_mean)
+
+#Choose target and features
 y = train_data.Survived
-features = ['Pclass', 'Age']
+features = ['Age', 'Pclass', 'Sex', 'Fare']
 X = train_data[features]
 
-# Split the data into training and validation data, for both feature and target
-train_X, val_X, train_y, val_y = train_test_split(X,y,random_state = 114514)
+# Train the model
+train_X, val_X, train_y, val_y = train_test_split(X,y,random_state=0)
 
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state=0)
+    model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state= 0)
     model.fit(train_X, train_y)
     preds_val = model.predict(val_X)
     mae = mean_absolute_error(val_y, preds_val)
@@ -36,11 +53,9 @@ for node_number in range(10, 200, 10):
         best_tree_size = node_number
     
 # Define & fit the model and make prediction
-final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size, random_state = 114514)
+final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size, random_state=0)
 final_model.fit(X, y)
 test_X = test_data[features]
-#print(test_X)
-
 predictions = final_model.predict(test_X)
 predictions = [round(num) for num in predictions]
 
